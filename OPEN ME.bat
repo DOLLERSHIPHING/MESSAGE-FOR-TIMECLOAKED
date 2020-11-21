0e1b28c56a8a409ef06c46f85c980e75a8d9a50b
DOLLERSHIPHING <70854720+DOLLERSHIPHING@users.noreply.github.com>
2020-11-21 16:34:41 +0100

    Add files via upload

---
+++ OPEN ME.bat
@@ -0,0 +1,85 @@
+
+@ECHO OFF
+cd .
+cd TALKSYSTEM
+color 02
+echo [INITIALIZING SYSTEMS]
+timeout 0 >nul
+echo [INI: 10%]
+timeout 0 >nul
+echo [INI: 23%]
+timeout 0 >nul
+echo [INI: 35%]
+timeout 0 >nul
+echo [INI: 47%]
+timeout 0 >nul
+echo [INI: 79%]
+timeout 0 >nul
+echo [INI: 85%]
+timeout 0 >nul
+echo [INI: 100%]
+timeout 0 >nul
+
+echo =============================
+echo LOADING MESSAGE TRANSCRIPT 
+ECHO =============================
+
+echo DS: HELLO TIMECLOAKED.
+DS hello timecloaked
+ECHO DS: YOU PATHETIC LOSER,
+DS you pathetic loser
+ECHO DS: I TRIED TO HELP YOU
+DS i tried to help you
+ECHO DS: BUT YOU JUST LEFT
+DS but you just left
+color 04
+ECHO DS: I STILL HATE YOU FOR THAT
+DS -pitch 167 -speed 76 -mouth 35 -throat 11 i still hate you for that
+color 02
+ECHO DS: YOU PUSSY
+DS you pussy.
+echo DS: YOU RAN AWAY.
+DS you ran away.
+echo DS: ANYWAYS, YOUR SHIT IS OVER, SUNNY BOY
+DS anyways, your shit is over, sunny boy.
+ECHO DS: YOU FUCKED UP FOR REAL THIS TIME.
+DS you fucked up for real this time
+ECHO DS: LEAVE SOCIAL MEDIA AND MOVE ON WITH YOUR SO CALLED 'LIFE'.
+DS leave social media and move on with your so called life
+ECHO DS: WHICH I THINK DOES NOT EXIST WHILST YOU PROBABLY QUOTE ON QUOTE,
+DS which i think does not exist whilst you probably quote on quote
+ECHO DS: KILLED YOURSELF :)
+DS killed yourself lmfao
+color 04
+ECHO DS: SMELLY NERD HAHAHAH
+DS -pitch 167 -speed 76 -mouth 35 -throat 11 smelly nerd HAHAHAH
+color 02
+ECHO DS: OK DUDE BUT FOR REAL, YOU FUCKED UP MANY THINGS AND MANY TIMES.
+DS ok dude but for real, you fucked up many THINGS AND MANY TIMES.
+ECHO DS: YOU CATFISHED WITH LUCAS J TAYLOR'S PICTURE HE GAVE YOU.
+DS you catfished with LUCAS J TAYLOR'S PICTURE HE GAVE YOU.
+ECHO DS: YOU BROKE PEOPLE'S HEARTS.
+DS you broke people's harts
+ECHO DS: YOU STOLE THINGS.
+DS you stole things
+ECHO DS: YOU EVEN WENT THAT LOW AND DESTROYED
+DS YOU EVEN WENT THAT LOW AND DESTROYED
+COLOR 04
+ECHO DS: THE ORIGINAL DOLLER TRREE!
+DS -pitch 167 -speed 76 -mouth 35 -throat 11 THE ORIGINAL DOLLER TRREE!
+color 02
+ECHO DS: YOU KNOW WHAT YOU DID. REALIZE IT.
+DS YOU KNOW WHAT YOU DID. REALIZE IT.
+ECHO DS: OK MAN, GO FUCK YOURSELF NOW. WITH MUCH LOVE
+DS OK MAN, GO FUCK YOURSELF NOW. WITH MUCH LOVE
+COLOR 04
+ECHO DS: DOLLERSHIPHING :)
+DS -pitch 167 -speed 76 -mouth 35 -throat 11 DOLLERSHIPHING
+color 02
+
+ECHO ENDING PROMPT.
+ECHO GOODBYE.
+
+
+timeout 10 >nul
+ECHO CLOSING.
